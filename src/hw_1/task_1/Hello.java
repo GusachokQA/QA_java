6f0ea073efa1489e6f71fa703ca5cbac447f855c
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-16 11:39:23 +0300

    Update Hello.java

--- src/hw_1/task_1/Hello.java
+++ src/hw_1/task_1/Hello.java
@@ -1,3 +1,6 @@
+// ты файл через идею создавал или копировал откуда-то? 
+// первой строкой всегда идет package, а я его здесь не вижу. попробуй создай заново
+
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Hello " + args[0]);
