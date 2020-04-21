f722ab28dff4f2cb0a7820551e25a43e562b8d76
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:44:15 +0300

    Update Task_1.java

--- src/hw_2/task_1/Task_1.java
+++ src/hw_2/task_1/Task_1.java
@@ -1,12 +1,14 @@
 package hw_2.task_1;
 
+// +
 public class Task_1 {
     public static void main(String[] args)  {
         double dayDistance = 10;
         double sum = 0;
+        // 7 в отдельную переменную
         for (int i = 0; i < 7; i++) {
             sum += dayDistance;
-            dayDistance *= 1.1;
+            dayDistance *= 1.1; // 1.1 в отдельную переменную
         }
         System.out.println("sum = " + sum);
     }
