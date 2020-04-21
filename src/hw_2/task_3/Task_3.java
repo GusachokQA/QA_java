de3727341859674ddf0459ca1eeba5ab77408d8f
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:46:06 +0300

    Update Task_3.java

--- src/hw_2/task_3/Task_3.java
+++ src/hw_2/task_3/Task_3.java
@@ -4,8 +4,10 @@ public class Task_3 {
     public static void main(String[] args) {
         int number = 1;
         int sum = 0;
+        // 256 в отдельную переменную
         while (number <= 256) {
            sum += number;
+            // 2 в отдельную переменную
            number *=2;
         }
         System.out.println("sum = " + sum);
