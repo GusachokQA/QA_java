d0300748557e99fc0f757189cae40ff445e395af
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:48:02 +0300

    Update Task_7.java

--- src/hw_2/task_7/Task_7.java
+++ src/hw_2/task_7/Task_7.java
@@ -3,6 +3,7 @@ package hw_2.task_7;
 public class Task_7 {
     public static void main(String[] args) {
         int sum = 0;
+        // 99 в отдельную переменную
         for (int i = 1; i <= 99; i++) {
             if (i % 2 != 0) {
                 sum += i;
