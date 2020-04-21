e950efd7dea0ea81c0393151b17ba8ebb31e9bb0
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:46:53 +0300

    Update Task_5.java

--- src/hw_2/task_5/Task_5.java
+++ src/hw_2/task_5/Task_5.java
@@ -4,6 +4,7 @@ public class Task_5 {
     public static void main(String[] args) {
         double cmInInch = 2.54;
 
+        // 20 в отдельную переменную
         for (int  i = 1; i <= 20; i++) {
             System.out.println("inch = " + i + " cm = " + cmInInch * i);
         }
