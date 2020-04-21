3352904ec0bd10e628081fd555fd93d59a0bd6f7
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-04-21 21:45:02 +0300

    Update Task_2.java

--- src/hw_2/task_2/Task_2.java
+++ src/hw_2/task_2/Task_2.java
@@ -3,9 +3,11 @@ package hw_2.task_2;
 public class Task_2 {
     public static void main(String[] args) {
         int ameba = 1;
-
+        // 24 в отдельную переменную
+        // 3 в отдельную переменную
         for (int i = 0; i <= 24; i += 3) {
             System.out.println("ameba = " + ameba + " hours = " + i);
+            // 2 в отдельную переменную
             ameba *= 2;
         }
     }
