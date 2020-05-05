61e06b134a8cf5f6cf24e4909d509ba3dde3af25
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-05 20:38:51 +0300

    Update Catalog.java

--- src/hw_5/task_3/Catalog.java
+++ src/hw_5/task_3/Catalog.java
@@ -4,6 +4,7 @@ public class Catalog {
     public House[] houses;
 
     public Catalog(){
+        // дома лучше передавать через конструктор
         houses = new House[4];
         houses[0] = new VillageHouse(1,4);
         houses[1] = new VillageHouse(1,3);
