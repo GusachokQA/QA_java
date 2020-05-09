995079f3b41d7a8385d0236055e3b4d8df755c52
Aleksandr Fomin <RezviyBelorus@gmail.com>
2020-05-09 13:01:09 +0300

    Update MonkeyBanana.java

--- src/hw_6/task_3/MonkeyBanana.java
+++ src/hw_6/task_3/MonkeyBanana.java
@@ -3,7 +3,8 @@ package hw_6.task_3;
 public class MonkeyBanana extends Monkey{
     @Override
     public int sumFruitCount(Tree tree) {
-        if (tree instanceof BananaTree){
+        if (tree instanceof BananaTree){ // instanceof используется в крайне редких случаях, это скорее недостаток, чем добро
+            // переопредели здесь метод sumFruitCount() и этот IF можно будет убрать
             return super.sumFruitCount(tree);
         }
         else {
